7346fd2cc3dc4e89cfae4b6daa73345115936166
Tingyuan Zhang <t.zhang@student.tue.nl>
2023-05-28 20:04:02 +0200

    api init

--- src/app/pixel-planet/utils/api.ts
+++ src/app/pixel-planet/utils/api.ts
@@ -1,3 +1,108 @@
-// import { API_CONFIG } from '../../../environments/environment';
+import { API_CONFIG } from '../../../environments/environment';
 
 // TODO: api implements
+
+import axios from 'axios';
+import { Player } from './player';
+import { Point } from './point';
+import * as moment from 'moment';
+
+axios.defaults.baseURL = API_CONFIG.baseUrl;
+axios.defaults.headers.common['Authorization'] = API_CONFIG.authentication;
+axios.defaults.timeout = 5000;
+const client = axios.create();
+export const API_getPlayer = async (playerId: number): Promise<Player> => {
+  const response = await client.get(`/players/${playerId}`);
+  const playerData = response.data as {
+    id: number;
+    user: {
+      email: string;
+      firstName: string;
+      lastName: string;
+    };
+  };
+  const player = new Player({ player_id: playerData.id });
+  // TODO: get planet and side, fake now
+  player.side = 'left';
+  player.planet_name = 'mars';
+  // End fake
+  return player;
+};
+type Challenge = { challengeId: number; startDate: Date; playerIds: number[] };
+export const API_getChallenge = async (
+  playerId: number
+): Promise<Challenge> => {
+  const response = await client.get(`/players/${playerId}/challenges/`);
+  const challengeData = response.data as {
+    id: number;
+    name: string;
+    startDate: number;
+    participations: { id: number }[];
+  }[];
+  const challenge = challengeData[challengeData.length - 1];
+  const playerIds = challenge.participations.map((p) => p.id);
+  console.log(new Date(challenge.startDate));
+  return {
+    challengeId: challenge.id,
+    startDate: new Date(challenge.startDate),
+    playerIds,
+  };
+};
+
+const API_getPoints = async (
+  playerId: number,
+  startDate: Date,
+  endDate: Date
+): Promise<Point[]> => {
+  const startDateString = moment(startDate).format('DD-MM-YYYY');
+  const endDateString = moment(endDate).format('DD-MM-YYYY');
+  const response = await client.get(
+    `/players/${playerId}/activities?start=${startDateString}&end=${endDateString}`
+  );
+  const activitiesData = response.data as {
+    id: number;
+    player: { id: number };
+    gameDescriptor: {
+      translationKey: string | 'WALK';
+    };
+    personalPoints?: {
+      value: number;
+    }[];
+    date: number;
+  }[];
+  const result = [] as Point[];
+  const date = moment(startDate);
+  while (true) {
+    if (date.isAfter(endDate)) break;
+    result.push(new Point({ date: date.toDate(), value: 0 }));
+    date.add(1, 'day');
+  }
+  activitiesData
+    .filter((activity) => {
+      if (activity.player.id !== playerId) return false;
+      return activity.gameDescriptor.translationKey === 'WALK';
+    })
+    .map((activity) => {
+      let value = 0;
+      if (activity.personalPoints) {
+        for (const point of activity.personalPoints) {
+          value += point.value;
+        }
+      }
+      return {
+        value,
+        date: new Date(activity.date),
+      };
+    })
+    .forEach((point) => {
+      const tmpDate = moment(point.date);
+      const index = result.findIndex((p) => {
+        return moment(p.date).isSame(tmpDate, 'day');
+      });
+      if (index !== -1) {
+        result[index].value += point.value;
+      }
+    });
+  console.log(result);
+  return result;
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,6 +16,7 @@ export const environment = {
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
 
 export const API_CONFIG = {
-  baseUrl: 'https://api-new.gamebus.eu/',
-  authentication: '',
+  baseUrl: 'https://api-new.gamebus.eu/v2/',
+  authentication:
+    'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiZ2FtZWJ1c19hcGkiXSwidXNlcl9uYW1lIjoiaW5mbytwbGFuZXR3YWxrZXIxQGdhbWVidXMuZXUiLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiLCJ0cnVzdCIsImRlbGV0ZTphY3Rpdml0eSJdLCJleHAiOjE3NjIxMDc3MDYsImF1dGhvcml0aWVzIjpbIlVTRVIiXSwianRpIjoiOHppMks3a3lqTmdfek1XVDJHbWxMeG5JOXh3IiwiY2xpZW50X2lkIjoiZ2FtZWJ1c19iYXNlX2FwcCJ9.Qw1ux1byWOhNeypAymqa7wxrQ2LZbD3lF8BdXcZXbhsTGlBO2wD-KhLY6RaFANUdc5tpHofSPb4dJlCKDkzjeFAnrGPx5uflePYd4G8-IJAmSxXse1Cn2itGCZ8jOWvckY21c82CN0nvqU7iBkECThYeBbPlVDENwo3Hn6liIRLeqivgohmrx9n7jJ4ReSXm28y1Hy65vggNCketKX6O1P4ywziMJFI16Swxt4P55NmezpYAc3rUkl_qGFqEjADS5jTb-Cyym02CGywCqSX2fe2f_3jKhK2I0jCTtS53xdl8xczhYnWYhPFy1ZEFzp7RlQIqyDIod8v06BF7F2W4dw',
 };
